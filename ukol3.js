72b2ce11ee20ed51cb8dba3878d233ee1c81bad9
katerinajelinkova1 <katerinajelinkova@outlook.com>
2022-01-22 14:30:05 +0100

    1 file modified

--- ukol3.js
+++ ukol3.js
@@ -1,1 +1,8 @@
-let cislo = prompt('zadej cislo more');
+let cislo = number(prompt('zadej cislo more'));
+let soucet = cislo;
+
+while (cislo != 0) {
+  console.log('soucet je ' + soucet);
+  cislo = number(prompt('zadej cislo more'));
+  soucet = soucet + cislo;
+}
